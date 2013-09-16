dceeec317644ad4aca1ad262f48690be34e0660d
Scott R. Johnston <wampiter@gmail.com>
2013-09-16 11:48:40 -0700

    Switched z data from piezo to monitor (2 to 3) to be consistent with everything else. Also made lift a function argument.

--- mim_absolute.m
+++ mim_absolute.m
@@ -1,14 +1,14 @@
-function [ mim_abs_map ] = mim_absolute(data,min,max)
+function [ mim_abs_map ] = mim_absolute(data,min,max,lift)
 %UNTITLED14 Summary of this function goes here
 %   Detailed explanation goes here
 addpath('common subfunctions')
 for i = 1:size(data,3)
-    z = data(:,2,i);
+    z = data(:,3,i);
     mim = data(:,6,i);
     
     maxind = index_of_bounded_max_d(z,mim,min,max);
     
-    [raiseind,~] = get_z_index(z,z(maxind),z(maxind)+0.02);
+    [raiseind,~] = get_z_index(z,z(maxind),z(maxind)+lift);
     
     mim_abs(i) = mim(maxind)-mim(raiseind);  
 end
